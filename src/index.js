047caa4f758e2d9b28c11df73f38c739500c48b3
jerryhuang3 <jerryhuang@utexas.edu>
2019-09-24 13:29:28 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import "./styles/styles.scss";
+
+const App = () => {
+  return <div>Hello World</div>;
+};
+
+render(<App />, document.getElementById("root"));
